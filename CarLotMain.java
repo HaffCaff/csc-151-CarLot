508e139cca2539773188579a42e9da8b8db4da19
Anthony E <AnthonyDEpps@gmail.com>
2023-11-05 10:36:32 -0500

    Skeleton of Menu, just need to add functionality

---
+++ CarLotMain.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class CarLotMain {
+    public static void main (String [] args){
+        int choice = 0;
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("[0] Exit");
+        System.out.println("[1] Add a car to inventory");
+        System.out.println("[2] Sell a car from inventory");
+        System.out.println("[3] List Inventory by rder of acquisition");
+        System.out.println("[4] List inventory from best MPG to Worst MPG");
+        System.out.println("[5] Display car with best MPG");
+        System.out.println("[6] Display car with highest mileage");
+        System.out.println("[7] Display overall MPG for the entire inventory");
+        System.out.println("[8] Display Profit for all sold cars");
+
+        System.out.print("Enter a number from 0 to 8");
+        choice = input.nextInt();
+
+        while (choice != 0){
+            if (choice == 1){
+                
+            }
+            else if (choice == 1){
+                
+            }
+            else if (choice == 2){
+                
+            }
+            else if (choice == 3){
+                
+            }
+            else if (choice == 4){
+                
+            }
+            else if (choice == 5){
+                
+            }
+            else if (choice == 6){
+                
+            }
+            else if (choice == 7){
+                
+            }
+            else if (choice == 8){
+                
+            }
+        }
+    }
+}
